8d21868ff8d398da2f94f4840325df06ab5ead48
Ice-Blck <carlosnoelg3@outlook.com>
2020-08-04 11:18:37 -0500

    Components - Badges List - Badges List Items

--- src/index.js
+++ src/index.js
@@ -1,32 +1,16 @@
-// const element = document.createElement('h1');
-// element.innerText = "Hello, Platzi Badges!";
-
-// const container = document.getElementById('app');
-
-// container.appendChild(element);
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import  './global.css';
 
-// const jsx = <h1>Hello, Platzi Badges from React!</h1>
-// const element = React.createElement(
-//     'a',
-//     { href: 'https://platzi.com'}, 
-//     'Ir a platzi!');
-// const name = "Carlos Noel Bernaola";
-// const sum = () => 3 + 3;
-// const element = React.createElement(
-//     'h1',
-//     {},
-//     `Hello, My name is ${name}`
-// );
-// const jsx = <h1>Hola soy, {sum()}</h1>;
-const jsx = (<div>
-    <h1>Hello, I'm Carlos Noel</h1>
-    <p>I'm a developer</p>
-</div>);
+import Badges from './pages/Badges';
+
+
 const container = document.getElementById('app');
 
 // ReactDOM.render(____qué___, ___donde__);
-ReactDOM.render(jsx, container);
+ReactDOM.render(
+<Badges
+/>, 
+container
+);
